49d452caed5109db8a106bf19116cad5d56cccc2
terraceo <halodek12y@gmail.com>
2024-10-10 16:27:35 +0700

    accesbot1

--- readme.js
+++ readme.js
@@ -1,2 +1,9 @@
-/// Jangan Di Sebar kan Agar Tidak Di Reset Paktzy
-/// Yg Rugi Sapa? Lu Sendiri 
+{
+    "creator": "https://wa.me/6282261282684",
+    "access": [
+      "6282241476058"
+    ],
+    "access_bot": [
+      "6282241476058"
+    ]
+}
